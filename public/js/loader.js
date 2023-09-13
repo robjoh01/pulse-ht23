9119d55a28f835871a8f1d35c95bfb345d6d2837
Robin Johannesson <robinjohannesson2@gmail.com>
2023-09-13 16:37:58 +0200

    Added dashboard and calendar.
    * Installed Toast UI Calendar package for handling the dashboard page.
    * Added new pages (dashboard, about, contact, profile, register and login).
    * Add new styles with sass.

--- public/js/loader.js
+++ public/js/loader.js
@@ -1,23 +1,85 @@
 "use strict";
 
-function updateTheme(matches) {
-    const link = document.getElementById("window-style");
-    const type = matches ? "dark" : "light";
-    link.setAttribute("href", `/styles/style-${type}.css`);
-}
+const Calendar = tui.Calendar;
 
-const mql = window.matchMedia("(prefers-color-scheme: dark)");
+// function updateTheme(matches) {
+//     const link = document.getElementById("window-style");
+//     const type = matches ? "dark" : "light";
+//     link.setAttribute("href", `/styles/style-${type}.css`);
+// }
 
-mql.addEventListener("change", e => {
-    updateTheme(e.matches);
-});
+// const mql = window.matchMedia("(prefers-color-scheme: dark)");
 
-updateTheme(mql.matches);
+// mql.addEventListener("change", e => {
+//     updateTheme(e.matches);
+// });
+
+// updateTheme(mql.matches);
 
 // mdc.ripple.MDCRipple.attachTo(document.querySelector<HTMLElement>('.foo-button'));
 
-const selector = '.mdc-button, .mdc-icon-button, .mdc-card__primary-action';
+// const selector = '.mdc-button, .mdc-icon-button, .mdc-card__primary-action';
 
-const ripples = [].map.call(document.querySelectorAll(selector), function(el) {
-    return new MDCRipple(el);
-});
+// const ripples = [].map.call(document.querySelectorAll(selector), function(el) {
+//     return new MDCRipple(el);
+// });
+
+// const togglePassword = document.querySelector("#togglePassword");
+// const password = document.querySelector("#password");
+
+// togglePassword.addEventListener("click", function () {
+//     // toggle the type attribute
+//     const type = password.getAttribute("type") === "password" ? "text" : "password";
+//     password.setAttribute("type", type);
+    
+//     // toggle the icon
+//     this.classList.toggle("bi-eye");
+// });
+
+// // prevent form submit
+// const form = document.querySelector("form");
+// form.addEventListener('submit', function (e) {
+//     e.preventDefault();
+// });
+
+const container = document.getElementById('calendar');
+
+const options = {
+    defaultView: 'month',
+    month: {
+        startDayOfWeek: 1,
+        visibleEventCount: 3,
+    },
+    gridSelection: {
+        enableDblClick: true,
+        enableClick: true,
+    },
+};
+
+const calendar = new Calendar(container, options);
+
+calendar.createEvents([
+    {
+        id: 'event1',
+        calendarId: 'cal2',
+        title: 'Weekly meeting',
+        start: '2023-09-14T09:00:00',
+        end: '2023-09-15T10:00:00',
+    },
+    {
+        id: 'event2',
+        calendarId: 'cal1',
+        title: 'Lunch appointment',
+        start: '2023-09-08T12:00:00',
+        end: '2023-09-08T13:00:00',
+    },
+    {
+        id: 'event3',
+        calendarId: 'cal2',
+        title: 'Vacation',
+        start: '2023-09-22',
+        end: '2023-10-03',
+        isAllday: true,
+        category: 'allday',
+    },
+]);
--- routes/routes.js
+++ routes/routes.js
@@ -7,18 +7,145 @@ const helpers = require("./../src/helpers.js");
 router.get("/", async (req, res) => {
     let data = {};
 
-    data.title = "Home";
+    data.title = "";
 
-    res.render("./../pages/index.ejs", data);
+    // Checks if you are logged in, if not, sign in. Otherwise, continue back on dashboard site.
+
+    res.redirect("/login");
+});
+
+router.get("/dashboard", (req, res) => {
+    let data = {};
+
+    data.title = "Dashboard";
+
+    res.render("./../pages/dashboard.ejs", data);
 });
 
 router.get("/about", async (req, res) => {
     let data = {};
 
     data.title = "About";
-    data.content = helpers.getAboutInfo();
 
     res.render("./../pages/about.ejs", data);
+});
+
+router.get("/contact", async (req, res) => {
+    let data = {};
+
+    data.title = "Contact";
+
+    res.render("./../pages/contact.ejs", data);
+});
+
+router.get("/error", (req, res) => {
+    let data = {};
+
+    data.title = "Error";
+
+    data.error_code = "404";
+    data.error_title = "Page Not Found ⚠️";
+    data.error_message = "We couldn′t find the page you are looking for.";
+
+    res.render("./../pages/error.ejs", data);
+});
+
+router.get("/register", (req, res) => {
+    let data = {};
+
+    data.title = "Register";
+
+    res.render("./../pages/register.ejs", data);
+});
+
+router.post("/register/posted", async (req, res) => {
+    const data = req.body;
+
+    console.log(data);
+
+    // let orderId = await helpers.createOrder(data.f_customer_id);
+
+    // let inventory = await helpers.getInventory();
+
+    // for (let i = 0; i < data.f_amount.length; i++) {
+    //     if (data.f_amount[i] <= 0) {
+    //         continue;
+    //     }
+
+    //     await helpers.addOrderRow(orderId, inventory[i].product_code, data.f_amount[i]);
+    // }
+
+    res.redirect("/profile");
+});
+
+router.get("/login", (req, res) => {
+    let data = {};
+
+    data.title = "Login";
+
+    res.render("./../pages/login.ejs", data);
+});
+
+router.post("/login/posted", async (req, res) => {
+    const data = req.body;
+
+    console.log(data);
+
+    // let orderId = await helpers.createOrder(data.f_customer_id);
+
+    // let inventory = await helpers.getInventory();
+
+    // for (let i = 0; i < data.f_amount.length; i++) {
+    //     if (data.f_amount[i] <= 0) {
+    //         continue;
+    //     }
+
+    //     await helpers.addOrderRow(orderId, inventory[i].product_code, data.f_amount[i]);
+    // }
+
+    res.redirect("/profile");
+});
+
+router.get("/logout", (req, res) => {
+    res.redirect("/login");
+});
+
+router.get("/profile", (req, res) => {
+    let data = {};
+
+    data.title = "Profile";
+
+    res.render("./../pages/profile.ejs", data);
+});
+
+router.get("/change_password", (req, res) => {
+    let data = {};
+
+    data.title = "Change Password";
+
+    // res.sendStatus(200);
+
+    res.render("./../pages/change_password.ejs", data);
+});
+
+router.post("/change_password/posted", async (req, res) => {
+    const data = req.body;
+
+    console.log(data);
+
+    // let orderId = await helpers.createOrder(data.f_customer_id);
+
+    // let inventory = await helpers.getInventory();
+
+    // for (let i = 0; i < data.f_amount.length; i++) {
+    //     if (data.f_amount[i] <= 0) {
+    //         continue;
+    //     }
+
+    //     await helpers.addOrderRow(orderId, inventory[i].product_code, data.f_amount[i]);
+    // }
+
+    res.redirect("/profile");
 });
 
 module.exports = router;
