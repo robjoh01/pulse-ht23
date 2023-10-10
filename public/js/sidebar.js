655216fa1d52ebc9bb5282bbda43649dbf1ab2e4
Robin Johannesson <robinjohannesson2@gmail.com>
2023-10-10 18:09:59 +0200

    Updated the website's stylehseet

--- public/js/sidebar.js
+++ public/js/sidebar.js
@@ -2,16 +2,53 @@
 
 const sidebarBtn = document.getElementById("sidebarBtn");
 const sidebar = document.getElementById("sidebar");
+const themeBtn = document.getElementById("themeBtn");
 
-if (localStorage.getItem("sidebar")) {
-    const isActive = localStorage.getItem("sidebar");
-
-    if (isActive === "true") {
-        sidebar.classList.add("active");
-    } else {
-        sidebar.classList.remove("active");
-    }
+if (overrideColorTheme) {
+    themeBtn.className = (overrideColorTheme === "dark") ? "light" : "dark";
+} else {
+    themeBtn.className = matchMedia.matches ? "light" : "dark";
 }
+
+matchMedia.addEventListener("change", e => {
+    if (overrideColorTheme) {
+        return;
+    }
+
+    themeBtn.className = e.matches ? "light" : "dark";
+});
+
+themeBtn.addEventListener("click", () => {
+    if (themeBtn.classList.contains("dark")) {
+        themeBtn.classList.remove("dark");
+        themeBtn.classList.add("light");
+
+        // Dark theme
+        document.body.classList.add("dark");
+        document.body.classList.remove("light");
+
+        localStorage.setItem("color-theme", "dark");
+    } else if (themeBtn.classList.contains("light")) {
+        themeBtn.classList.remove("light");
+        themeBtn.classList.add("dark");
+
+        // Light light
+        document.body.classList.add("light");
+        document.body.classList.remove("dark");
+
+        localStorage.setItem("color-theme", "light");
+    }
+});
+
+// if (localStorage.getItem("sidebar")) {
+//     const isActive = localStorage.getItem("sidebar");
+
+//     if (isActive === "true") {
+//         sidebar.classList.add("active");
+//     } else {
+//         sidebar.classList.remove("active");
+//     }
+// }
 
 sidebarBtn.addEventListener("click", () => {
     if (sidebar.classList.contains("active")) {
--- public/js/theme.js
+++ public/js/theme.js
@@ -1,43 +1,10 @@
 "use strict";
 
-const themeToggleBtn = document.getElementById("themeBtn");
-
-const matchMedia = window.matchMedia?.("(prefers-color-scheme: dark)");
-themeToggleBtn.className = matchMedia.matches ? "light" : "dark";
-
-const overrideColorTheme = localStorage.getItem("color-theme");
+let matchMedia = window.matchMedia?.("(prefers-color-scheme: dark)");
+let overrideColorTheme = localStorage.getItem("color-theme");
 
 if (overrideColorTheme) {
     document.body.className = overrideColorTheme;
-    themeToggleBtn.className = (overrideColorTheme === "dark") ? "light" : "dark";
+} else {
+    document.body.className = matchMedia.matches ? "light" : "dark";
 }
-
-matchMedia.addEventListener("change", e => {
-    if (overrideColorTheme) {
-        return;
-    }
-
-    themeToggleBtn.className = e.matches ? "light" : "dark";
-});
-
-themeToggleBtn.addEventListener("click", () => {
-    if (themeToggleBtn.classList.contains("dark")) {
-        themeToggleBtn.classList.remove("dark");
-        themeToggleBtn.classList.add("light");
-
-        // Dark theme
-        document.body.classList.add("dark");
-        document.body.classList.remove("light");
-
-        localStorage.setItem("color-theme", "dark");
-    } else if (themeToggleBtn.classList.contains("light")) {
-        themeToggleBtn.classList.remove("light");
-        themeToggleBtn.classList.add("dark");
-
-        // Light light
-        document.body.classList.add("light");
-        document.body.classList.remove("dark");
-
-        localStorage.setItem("color-theme", "light");
-    }
-});
